2a5d12ed5bd28a2e153a26a52102c50d983a9f43
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2024-01-03 10:00:39 +1100

    oops

--- tools/tasks/shared.ts
+++ tools/tasks/shared.ts
@@ -22,7 +22,7 @@ async function zipOrCopySpecifiedModule(zip: boolean) {
 	if (!module) throw new Error("Module Env Variable must not be empty.");
 	if (module === buildConfig.combinedName) return zipOrCopyCombined(zip);
 	if (!(module in modulesFile.modules))
-		throw new Error("Module Env Variable must be a module specified in module.json, or 'combined'.");
+		throw new Error(`Module Env Variable must be a module specified in module.json, or 'combined'. Found: ${module}.`);
 
 	return zipOrCopyModule(zip, module);
 }
